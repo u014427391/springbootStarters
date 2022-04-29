6a589bf10b21b7a826394d1a81adbafb3584d1ad
nicky <2284087296@qq.com>
2022-04-29 17:52:18 +0800

    resttemplate starter

---
+++ resttemplate-spring-boot-starter/src/test/java/com/example/resttemplate/ResttemplateSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttemplateSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
