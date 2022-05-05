4a034d29b5592b1d583ee9bdc62e8d09236f6022
nicky <2284087296@qq.com>
2022-05-05 08:54:02 +0800

    resttemplate starter

--- resttemplate-spring-boot-starter/src/main/java/org/framework/starter/resttemplate/ResttemplateSpringBootStarterApplication.java
+++ resttemplate-spring-boot-starter/src/main/java/org/framework/starter/resttemplate/ResttemplateSpringBootStarterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResttemplateSpringBootStarterApplication {
+public class RestTemplateSpringBootStarterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ResttemplateSpringBootStarterApplication.class, args);
+        SpringApplication.run(RestTemplateSpringBootStarterApplication.class, args);
     }
 
 }
