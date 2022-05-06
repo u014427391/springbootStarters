aecf383d815de07a2fea4dcae467e0f6edad7638
nicky <2284087296@qq.com>
2022-05-06 10:40:10 +0800

    resttemplate starter

---
+++ resttemplate-spring-boot-starter/src/main/java/org/framework/starter/resttemplate/configuration/HttpClientPoolProperties.java
@@ -0,0 +1,63 @@
+package org.framework.starter.resttemplate.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "http-client.pool")
+@Data
+public class HttpClientPoolProperties {
+
+    /**
+     *  是否开启，默认不开启
+     */
+    private Boolean enabled = Boolean.FALSE;
+
+    /**
+     * 连接池的最大连接数
+     */
+    private int maxTotalConnect ;
+
+    /**
+     * 同路由的并发数
+     */
+    private int maxConnectPerRoute;
+
+    /**
+     * 客户端和服务器建立连接超时，默认2s
+     */
+    private int connectTimeout = 2 * 1000;
+
+    /**
+     * 指客户端从服务器读取数据包的间隔超时时间,不是总读取时间，默认30s
+     */
+    private int readTimeout = 30 * 1000;
+
+    /**
+     *  字符集
+     */
+    private String charset = "UTF-8";
+
+    /**
+     * 重试次数,默认2次
+     */
+    private int retryTimes = 2;
+
+    /**
+     * 从连接池获取连接的超时时间,单位ms
+     */
+    private int connectionRequestTimeout = 200;
+
+    /**
+     * 针对不同的地址,特别设置不同的长连接保持时间
+     */
+    private Map<String, Integer> keepAliveTargetHost;
+
+    /**
+     * 针对不同的地址,特别设置不同的长连接保持时间,单位 s
+     */
+    private int keepAliveTime = 60;
+
+
+}
--- resttemplate-spring-boot-starter/src/main/java/org/framework/starter/resttemplate/configuration/RestTemplateAutoConfiguration.java
+++ resttemplate-spring-boot-starter/src/main/java/org/framework/starter/resttemplate/configuration/RestTemplateAutoConfiguration.java
@@ -1,15 +1,61 @@
 package org.framework.starter.resttemplate.configuration;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.Header;
+import org.apache.http.HeaderElement;
+import org.apache.http.HeaderElementIterator;
+import org.apache.http.HttpHost;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicHeaderElementIterator;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
 @Configuration
+@ConditionalOnProperty(prefix = "http-client.pool" , name = "enabled" , havingValue = "true")
+@ConditionalOnClass({HttpClientPoolProperties.class,RestTemplate.class,CloseableHttpClient.class})
+@EnableConfigurationProperties({HttpClientPoolProperties.class})
+@Slf4j
 public class RestTemplateAutoConfiguration {
+
+    private final HttpClientPoolProperties httpClientPoolProperties;
+
+    public RestTemplateAutoConfiguration(HttpClientPoolProperties httpClientPoolProperties) {
+        this.httpClientPoolProperties = httpClientPoolProperties;
+    }
 
     @Bean
     public CustomRestTemplateCustomizer customRestTemplateCustomizer() {
@@ -23,20 +69,136 @@ public class RestTemplateAutoConfiguration {
     }
 
     @Bean
-    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        //单位为ms
-        factory.setReadTimeout(5000);
-        //单位为ms
-        factory.setConnectTimeout(5000);
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        if (httpClientPoolProperties.getMaxTotalConnect() <= 0) {
+            throw new IllegalArgumentException("invalid maxTotalConnection: " + httpClientPoolProperties.getMaxTotalConnect());
+        }
+        if (httpClientPoolProperties.getMaxConnectPerRoute() <= 0) {
+            throw new IllegalArgumentException("invalid maxConnectionPerRoute: " + httpClientPoolProperties.getMaxConnectPerRoute());
+        }
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory (httpClient());
+        // 数据读取超时时间，即SocketTimeout
+        factory.setReadTimeout(httpClientPoolProperties.getReadTimeout());
+        // 连接超时
+        factory.setConnectTimeout(httpClientPoolProperties.getConnectTimeout());
+        // 请求连接的超时时间
+        factory.setConnectionRequestTimeout(httpClientPoolProperties.getConnectionRequestTimeout());
         return factory;
     }
 
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder ,ClientHttpRequestFactory factory ) {
+    public RestTemplate restTemplate(RestTemplateBuilder builder ,ClientHttpRequestFactory factory) {
         RestTemplate restTemplate = builder.build();
         restTemplate.setRequestFactory(factory);
+        //设置错误处理器
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler());
+        // 重新设置StringHttpMessageConverter字符集
+        modifyDefaultCharset(restTemplate);
         return restTemplate;
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+        try {
+            //设置信任ssl访问
+            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, (arg0, arg1) -> true).build();
+
+            httpClientBuilder.setSSLContext(sslContext);
+            HostnameVerifier hostnameVerifier = NoopHostnameVerifier.INSTANCE;
+            SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                    // 注册http和https请求
+                    .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                    .register("https", sslConnectionSocketFactory).build();
+
+            //使用Httpclient连接池的方式配置(推荐)，同时支持netty，okHttp以及其他http框架
+            PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+            // 最大连接数
+            poolingHttpClientConnectionManager.setMaxTotal(httpClientPoolProperties.getMaxTotalConnect());
+            // 同路由并发数
+            poolingHttpClientConnectionManager.setDefaultMaxPerRoute(httpClientPoolProperties.getMaxConnectPerRoute());
+            //配置连接池
+            httpClientBuilder.setConnectionManager(poolingHttpClientConnectionManager);
+            // 重试次数
+            httpClientBuilder.setRetryHandler(new DefaultHttpRequestRetryHandler(httpClientPoolProperties.getRetryTimes(), true));
+
+            //设置默认请求头
+            List<Header> headers = getDefaultHeaders();
+            httpClientBuilder.setDefaultHeaders(headers);
+            //设置长连接保持策略
+            httpClientBuilder.setKeepAliveStrategy(connectionKeepAliveStrategy());
+            return httpClientBuilder.build();
+        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+            log.error("HttpClient exception {}", e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * 配置长连接保持策略 <br>
+     * @Date 2022/05/06 9:57
+     * @return org.apache.http.conn.ConnectionKeepAliveStrategy
+     */
+    public ConnectionKeepAliveStrategy connectionKeepAliveStrategy(){
+        return (response, context) -> {
+            // 'keep-alive' header
+            HeaderElementIterator it = new BasicHeaderElementIterator(
+                    response.headerIterator(HTTP.CONN_KEEP_ALIVE));
+            while (it.hasNext()) {
+                HeaderElement he = it.nextElement();
+                String param = he.getName();
+                String value = he.getValue();
+                if (value != null && "timeout".equalsIgnoreCase(param)) {
+                    try {
+                        return Long.parseLong(value) * 1000;
+                    } catch(NumberFormatException e) {
+                        log.error("NumberFormatException {}", e.getMessage());
+                    }
+                }
+            }
+            HttpHost target = (HttpHost) context.getAttribute(HttpClientContext.HTTP_TARGET_HOST);
+            //如果请求目标地址,单独配置了长连接保持时间,使用该配置
+            Optional<Map.Entry<String, Integer>> any = Optional.ofNullable(httpClientPoolProperties.getKeepAliveTargetHost())
+                    .orElseGet(HashMap::new)
+                    .entrySet().stream()
+                    .filter(e -> e.getKey().equalsIgnoreCase(target.getHostName()))
+                    .findAny();
+            //否则使用默认长连接保持时间
+            return any.map(en -> en.getValue() * 1000L)
+                    .orElse(httpClientPoolProperties.getKeepAliveTime() * 1000L);
+        };
+    }
+
+    /**
+     * 设置请求头
+     *
+     * @return 请求头
+     */
+    private List<Header> getDefaultHeaders() {
+        List<Header> headers = new ArrayList<>();
+        headers.add(new BasicHeader("User-Agent",
+                "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36"));
+        headers.add(new BasicHeader("Accept-Encoding", "gzip,deflate"));
+        headers.add(new BasicHeader("Accept-Language", "zh-CN"));
+        headers.add(new BasicHeader("Connection", "Keep-Alive"));
+        return headers;
+    }
+
+    private void modifyDefaultCharset(RestTemplate restTemplate) {
+        List<HttpMessageConverter<?>> converterList = restTemplate.getMessageConverters();
+        HttpMessageConverter<?> converterTarget = null;
+        for (HttpMessageConverter<?> item : converterList) {
+            if (StringHttpMessageConverter.class == item.getClass()) {
+                converterTarget = item;
+                break;
+            }
+        }
+        if (null != converterTarget) {
+            converterList.remove(converterTarget);
+        }
+        Charset defaultCharset = Charset.forName(httpClientPoolProperties.getCharset());
+        converterList.add(1, new StringHttpMessageConverter(defaultCharset));
     }
 
 }
